91893f25e7d1ac878bb1e2c084381ff8e95bef1b
Akshvar3 <g.akshith13@gmail.com>
2025-09-20 14:25:49 -0400

    Icons to main app + added img files

--- Main_Application.py
+++ Main_Application.py
@@ -6,28 +6,30 @@ class MainApp:
     def __init__(self):
         self.app_window = Tk()
         self.app_window.title("Main Application")
-        self.app_window.geometry("800x600")
+        self.app_window.geometry("617x660")
         self.create_widgets()
         self.app_window.mainloop()
 
     def create_widgets(self):
-        check_budg_img = Image.open('Check_Budget_Icon.jpeg').resize((200, 200))
+        check_budg_img = Image.open('Check_Budget_Icon.jpg').resize((300, 300))
         check_budg_icon = ImageTk.PhotoImage(check_budg_img)
         check_budg_button = Button(self.app_window, image=check_budg_icon, text="Budget Tracker", compound=TOP) #, command=self.open_budget)
         check_budg_button.image = check_budg_icon
         check_budg_button.grid(row=0, column=0, padx=0, pady=0)
+        '''
 
-        add_spending_img = Image.open('Add_Spending_Icon.jpeg').resize((200, 200))
+        add_spending_img = Image.open('Add_Spending_Icon.jpeg').resize((300, 300))
         add_spending_icon = ImageTk.PhotoImage(add_spending_img)
         add_spending_button = Button(self.app_window, image=add_spending_icon, text="Modify Categories", compound=TOP) #, command=self.open_spending)
         add_spending_button.image = add_spending_icon
         add_spending_button.grid(row=0, column=1, padx=0, pady=0)
+        '''
 
-        calc_icon_img = Image.open('Calculator_Icon.jpeg').resize((200, 200))
+        calc_icon_img = Image.open('Calculator_Icon.png').resize((300, 300))
         calc_icon = ImageTk.PhotoImage(calc_icon_img)
         calc_button = Button(self.app_window, image=calc_icon, text="Calculator App", compound=TOP) #, command=self.open_calculator)
         calc_button.image = calc_icon
-        calc_button.grid(row=0, column=2, padx=0, pady=0)
+        calc_button.grid(row=1, column=1, padx=0, pady=0)
 
 
         
