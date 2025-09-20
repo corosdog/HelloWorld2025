4e9bca126bbcf08b9f9598c5729ca19b292891b9
corosdog <corinwithers0@gmail.com>
2025-09-20 18:44:45 -0400

    Added comments to budget functions

--- budget.py
+++ budget.py
@@ -1,6 +1,7 @@
 import spending_type
 
 class budget:
+    # Constructor for the budget type
     def __init__(self, budget_name, monthly_budget):
         self.budget_name = budget_name
         self.monthly_budget = monthly_budget
@@ -14,13 +15,17 @@ class budget:
     def get_monthly_budget(self):
         return self.monthly_budget
 
+    # Adds a new spending category object to the budget's array of spending objects,
+    # and then removes the money allocated to the new spending category from the unallocated spending spending_type
     def add_new_spending_category(self, spending_category):
         self.spending_categories.append(spending_category)
         self.spending_categories[0].set_amount(self.spending_categories[0].get_amount() - spending_category.get_amount())
 
+    # Returns a spending category object from the specified index
     def get_spending_category(self, category_index):
         return self.spending_categories[category_index]
 
+    # Removes a spending_category from the list and frees up some unallocated funds
     def remove_spending_category(self, category_index):
         if category_index == 0:
             print("You cannot remove the unallocated spending category")
@@ -30,6 +35,7 @@ class budget:
         self.spending_categories[0].set_amount(self.spending_categories[0].get_amount() + self.spending_categories[category_index].get_amount())
         self.spending_categories.pop(category_index)
 
+    # Calls spending_category spend function and adds amount spent to total spent and subtracts amount spent from total remaining
     def spend_money(self, spending_category, amount):
         spending_category.spend(amount)
         self.total_remaining -= amount
